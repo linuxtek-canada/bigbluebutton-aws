054f6dfc12b7404b5b5784d07dcc7f83a4f995d4
Jason Paul <jason@linuxtek.ca>
2025-03-02 17:40:00 -0500

    Fixing module name in output

--- environments/prod/output.tf
+++ environments/prod/output.tf
@@ -1,4 +1,4 @@
 output "state_bucket_arn" {
   description = "The Terraform state bucket where this configuration is stored"
-  value       = module.remote_state.s3_bucket_arn
+  value       = module.terraform_state_backend.s3_bucket_arn
 }
